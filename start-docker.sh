441799e5d68bb9bfe4a9e86709e6126eb9eb3189
Ilia Smirnov <php1602agregator@gmail.com>
2024-09-21 02:39:03 +0300

    Update start-docker.sh

--- start-docker.sh
+++ start-docker.sh
@@ -6,5 +6,14 @@ cd doccano
 cd docker
 cp .env.example .env
 #docker run -v /var/run/docker.sock:/var/run/docker.sock docker:latest
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock docker:latest version
-docker-compose -f docker-compose.prod.yml --env-file .env up
+#docker run --rm -v /var/run/docker.sock:/var/run/docker.sock docker:latest version
+#docker-compose -f docker-compose.prod.yml --env-file .env up
+docker pull doccano/doccano
+docker container create --name doccano \
+  -e "ADMIN_USERNAME=admin" \
+  -e "ADMIN_EMAIL=admin@example.com" \
+  -e "ADMIN_PASSWORD=password" \
+  -v doccano-db:/data \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -p 80:8000 doccano/doccano
+docker container start doccano
