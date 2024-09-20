e00ca370650060fd0e62514f90cbe6521360ac5f
Ilia Smirnov <php1602agregator@gmail.com>
2024-09-20 20:15:29 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+systemctl enable docker
+service docker start
+# Install docker compose
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+# Install git
+yum install -y git
+git clone https://github.com/doccano/doccano.git
+cd doccano
+cp docker/.env.example .env
+docker-compose -f docker/docker-compose.prod.yml up -d
