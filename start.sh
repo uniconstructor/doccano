055c265c8e14bc5c879b28f069fb88b10f757f69
Ilia Smirnov <php1602agregator@gmail.com>
2024-09-20 20:37:07 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ yum update -y
 # Install Docker
 yum install -y docker
 systemctl enable docker
-systemctl docker start
+systemctl start docker
 # Install docker compose
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
