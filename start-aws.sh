2908afca1a024c3be061c2ddac78098a66ff342a
Ilia Smirnov <php1602agregator@gmail.com>
2024-09-21 11:48:25 +0300

    Update start-aws.sh

--- start-aws.sh
+++ start-aws.sh
@@ -5,7 +5,8 @@ yum install -y initscripts
 yum install -y docker
 systemctl enable docker
 export PATH=$PATH:/bin:/usr/local/bin
-service docker start
+#service docker start
+systemctl start docker
 # Install docker compose
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
