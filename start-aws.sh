38efa4b51a6ec7941b73a4ab4bc57a6a1217a848
Ilia Smirnov <php1602agregator@gmail.com>
2024-09-21 11:51:13 +0300

    Update start-aws.sh

--- start-aws.sh
+++ start-aws.sh
@@ -8,7 +8,7 @@ export PATH=$PATH:/bin:/usr/local/bin
 #service docker start
 systemctl start docker
 # Install docker compose
-curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.29.7/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 # Install git
