adbe9284158c43753a856ecd8f302dede747b485
Ilia Smirnov <php1602agregator@gmail.com>
2024-09-21 13:18:17 +0300

    Update start-docker.sh

--- start-docker.sh
+++ start-docker.sh
@@ -28,7 +28,9 @@ echo \
 apt-get update
 
 #curl -fsSL https://get.docker.com/rootless | sh
-apt-get install -y docker docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose docker-compose-plugin docker-ce-rootless-extras
+apt-get install -y docker #docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose docker-compose-plugin docker-ce-rootless-extras
+apt-get install -y docker-compose
+apt-get install -y docker-compose-plugin
 dockerd-rootless-setuptool.sh install
 #export PATH=/home/$USER/bin:$PATH
 #export DOCKER_HOST=unix:///run/$USER/1000/docker.sock
